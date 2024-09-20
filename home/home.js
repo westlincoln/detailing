d431c83c8090f7fbc8a919a709a11bf2ada9659a
westlincoln <westlincolndetailing@gmail.com>
2024-09-20 15:52:26 -0400

    Add files via upload

---
+++ home/home.js
@@ -0,0 +1,9 @@
+
+  document.getElementById('booknow').addEventListener('click', function() {
+    window.location.href = '/booking/booking.html'; // Redirect to the booking page
+  });
+
+  document.getElementById('quoteestimation').addEventListener('click', function() {
+    window.location.href = '/index.html'; // Redirect to the quote estimation page
+  });
+
